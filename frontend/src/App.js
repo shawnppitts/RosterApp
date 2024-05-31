a779b4cbe1b6b03ccc95e981318aff1018b9a5f7
Shawn Pitts <shawnpitts@Shawns-MacBook-Pro.local>
2024-05-30 21:06:28 -0400

    cleanup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,88 @@
 import logo from './logo.svg';
+import 'semantic-ui-css/semantic.min.css'
+import React, { useState } from 'react'
+import { 
+  Dropdown, 
+  Button,   
+  TableRow,
+  TableHeaderCell,
+  TableHeader,
+  TableCell,
+  TableBody,
+  Table, 
+} from 'semantic-ui-react'
 import './App.css';
+import axios from 'axios'
+
+// MLB Teams
+const teams = [
+  {text:'Boston Red Sox', value:'redsox'},
+  {text:'New York Yankees', value:'yankees'}
+]
 
 function App() {
+
+  const [team, setTeam] = useState(0)
+  const [roster, setRoster] = useState(0)
+  const [error, setError] = useState(0)
+
+  const handleChange = (event, data) => {
+    setTeam(data.value)
+  }
+
+  const fetchRoster = async () => {
+    if (team !== 0){
+      try {
+        const url = `http://127.0.0.1:8080/roster/${team}`
+        const response = await axios.get(url);
+        setRoster(response);
+      } catch (error) {
+        setError(error);
+      }
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>MLB Roster</h1>
+      <Dropdown
+        placeholder='Select Team'
+        fluid
+        selection
+        options={teams}
+        onChange={handleChange}
+      />
+      <Button onClick={fetchRoster}>Get Roster</Button>
+      <div>
+      {roster ? (
+        <div>
+          <Table celled inverted selectable>
+            <TableHeader>
+              <TableRow>
+                <TableHeaderCell>Name</TableHeaderCell>
+              </TableRow>
+            </TableHeader>
+
+            <TableBody>
+                {roster["data"].map((item, index) => (
+                  <TableRow>
+                    <TableCell key={index}>{item}</TableCell>
+                  </TableRow>
+                ))}                
+            </TableBody>
+          </Table>       
+        </div>
+      ) : error ? (
+        <div>
+          <h1>Error:</h1>
+          <pre>{JSON.stringify(error, null, 2)}</pre>
+        </div>
+      ) : (
+        <h1>Loading...</h1>
+      )}
     </div>
-  );
+    </div>
+  )
 }
 
 export default App;
